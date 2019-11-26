6434ccb0f864cf3106060aa0c3c7a848bcc6449d
daviavmello <davimello1994@gmail.com>
2019-11-25 23:55:20 -0500

    Added divs that work with bootstrap breakpoints

--- js/my.js
+++ js/my.js
@@ -1,8 +1,23 @@
     console.log("I can see you! Welcome to my website 🙃");
 
     $(document).ready(function() {
-      $('.icon').on('click', function(){
+
+      $(window).resize(() => {
+        var width = $(window).width();
+          if (width < '576') {
+              $('#hero-bg').hide();
+              $('.col-6').removeClass('col-6').addClass('col-lg');
+          }
+          else if (width >= '576') {
+              $('#hero-bg').show();
+              $('#hero').removeClass('col-lg').addClass('col-6');
+          }
+      });
+
+    $('.icon').on('click', function(){
         $('.icon').toggleClass('active');
         $('.categories').toggleClass('active');
       })
+
     });
+    

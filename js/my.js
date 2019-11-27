13d2e3966cc9ba1abbd5623b6eb9db5b66a6e83a
daviavmello <davimello1994@gmail.com>
2019-11-27 18:22:39 -0500

    Added fade in to H1

--- js/my.js
+++ js/my.js
@@ -1,27 +1,30 @@
 console.log("I can see you! Welcome to my website 🙃");
 
-/* Open mobile menu */
+// Open mobile menu
 function openNav() {
   $(document.getElementById("myNav")).css("width", "100%");
   $(".icon-mobile").toggleClass("active");
 }
 
-/* Close mobile menu */
+// Close mobile menu
 function closeNav() {
   $(document.getElementById("myNav")).css("width", "0%");
   $(".icon-mobile").toggleClass("active");
 }
 
+// Mobile window
 $(document).ready(function() {
+
+  $("h1").fadeIn(4000).removeClass("d-none");
+  
   $(window).resize(() => {
     var width = $(window).width();
     if (width < 768) {
       $(".col-6").removeClass("col-6").addClass("col-lg");
-      openNav();
-      closeNav();
     }
   });
 
+// Desktop hamburger menu
   $(".icon").on("click", function() {
     $(".icon").toggleClass("active");
     $(".categories").toggleClass("active");

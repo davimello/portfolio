ccc3a4fea29e19fcf22554f746e33bea45091958
daviavmello <davimello1994@gmail.com>
2019-11-27 17:31:04 -0500

    Added new menu for mobile

--- js/my.js
+++ js/my.js
@@ -1,29 +1,30 @@
-    console.log("I can see you! Welcome to my website 🙃");
+console.log("I can see you! Welcome to my website 🙃");
 
-    $(document).ready(function() {
+/* Open mobile menu */
+function openNav() {
+  $(document.getElementById("myNav")).css("width", "100%");
+  $(".icon-mobile").toggleClass("active");
+}
 
-    $('.icon').on('click', function(){
-        $('.icon').toggleClass('active');
-        $('.categories').toggleClass('active');
-      })
+/* Close mobile menu */
+function closeNav() {
+  $(document.getElementById("myNav")).css("width", "0%");
+  $(".icon-mobile").toggleClass("active");
+}
 
-    // function resize() {
-    //   if (window.matchMedia('(max-width: 767px)').matches) {
-    //     $('#hero').removeClass('col-6').addClass('col-lg');
-    // }};
-    
-    // resize();
+$(document).ready(function() {
+  $(window).resize(() => {
+    var width = $(window).width();
+    if (width < 768) {
+      $(".col-6").removeClass("col-6").addClass("col-lg");
+      openNav();
+      closeNav();
+    }
+  });
 
-      // $(window).resize(() => {
-      //   var width = $(window).width();
-      //     if (width < 768) {
-      //         // $('#hero-bg').hide();
-      //         $('.col-6').removeClass('col-6').addClass('col-lg');
-      //     }
-      //     else {
-      //         // $('#hero-bg').show();
-      //         $('#hero').removeClass('col-lg').addClass('col-6');
-      //     }
-      // });
+  $(".icon").on("click", function() {
+    $(".icon").toggleClass("active");
+    $(".categories").toggleClass("active");
+  });
 
-    });
+});

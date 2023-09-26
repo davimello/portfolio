0dc3516015ca7007feecaf91f2853023ff66c17a
Davi Mello <davimello1994@gmail.com>
2023-09-26 19:20:15 -0400

    Implementing lazy loading in App.component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactComponent } from './contact/contact.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./app.component').then((mod) => mod.AppComponent),
+  },
   { path: 'contact', component: ContactComponent },
   { path: '**', redirectTo: '/' },
 ];

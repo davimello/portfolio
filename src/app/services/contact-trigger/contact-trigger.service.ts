1402795a48947390d0970a2f90f538589ccf3d33
Davi Mello <davimello1994@gmail.com>
2023-10-05 14:25:22 -0400

    Removing console.log

--- src/app/services/contact-trigger/contact-trigger.service.ts
+++ src/app/services/contact-trigger/contact-trigger.service.ts
@@ -9,8 +9,6 @@ export class ContactTriggerService {
   openContactComponent$: Observable<boolean> = this.openContactComponent.asObservable();
 
   open(): void {
-    console.log('open');
-    
     this.openContactComponent.next(true);
   }
 
